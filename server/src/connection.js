b858a4c20faf7d19c51f699a45b9ca797a015eba
Andre Williams <51215949+williamsa3773@users.noreply.github.com>
2024-08-03 13:43:34 -0400

    fixed mongo connection issue'

--- server/src/connection.js
+++ server/src/connection.js
@@ -1,26 +1,31 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const dotenv = require('dotenv').config();
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env'});
 
 const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, {
 	serverApi: {
-	  version: ServerApiVersion.v1,
-	  strict: true,
-	  deprecationErrors: true,
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
 	}
-  });
-  async function run() {
+});
+
+async function run() {
 	try {
-	  // Connect the client to the server	(optional starting in v4.7)
-	  await client.connect();
-	  // Send a ping to confirm a successful connection
-	  await client.db("admin").command({ ping: 1 });
-	  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-	} finally {
-	  // Ensures that the client will close when you finish/error
-	  await client.close();
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("anwilliams").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} catch(err) {
+		console.error(err);
 	}
-  }
-  run().catch(console.dir);
+}
+
+let db = client.db("monarch");
+
+export default  db;
 
