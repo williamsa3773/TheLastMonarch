5040c647bce08bf5accc8d80544895467e624b46
Dre <williamsa3773@gmail.com>
2024-08-03 13:04:34 -0400

    setup mongo connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,40 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const uri = process.env.MONGO_URI;
-const mongoUser = process.env.MONGO_USERNAME;
-
-const client = new MongoClient(uri, {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
-	}
-});
-
-async function run() {
-	try	
-	{
-		await client.connect();
-		await client.db(mongoUser).command({ ping: 1});
-		console.log("Pinged your deployment");
-	} finally {
-		await client.close();
-	}
-}
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', async (req, res) => {
-	try {
-		await run();
-		res.send('Express server for The Last Monarch');
-	} catch (error) {
-		console.error(error)
-		res.status(500).send('Error connecting to the database')
-	}
+	res.send('Express server for The Last Monarch');
 });
 
 app.listen(port, () => {
